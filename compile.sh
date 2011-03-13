d464c68b9f17414f120f0193b11a66a66bcfff8b
ppershing <ppershing@c9fae5e5-1018-74da-50a3-0de90486fdfd>
2011-03-13 13:10:17 +0000

    Presuvavam subory.
    git-svn-id: http://ksp-hell-world.googlecode.com/svn@5 c9fae5e5-1018-74da-50a3-0de90486fdfd

--- compile.sh
+++ compile.sh
@@ -1,3 +1,18 @@
-cat $@ | ./preprocess.sh > __.cpp;
-g++ __.cpp -o hell 2> __.err ||
-  (cat __.err | ./recover_errormsg.sh && exit 1)
+if [ $# -ne 1 ]; then
+    echo "Usage: compile.sh filename"
+    exit 1
+fi
+
+filename="$1"
+if [ "${filename: -6}" == ".hellc" ]; then
+    cat "$filename" | ./preprocess_cpp.sh > __.cpp;
+    g++ __.cpp -o hell 2> __.err ||
+      (cat __.err | ./recover_errormsg.sh && exit 1)
+elif [ "${filename: -6}" == ".hellp" ]; then
+    cat "$filename" | ./preprocess_pas.sh > __.pas;
+    fpc __.pas -ohell 2> __.err ||
+      (cat __.err | ./recover_errormsg.sh && exit 1)
+else
+    echo "Unknown extension";
+    exit 1;
+fi
