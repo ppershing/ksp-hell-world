a2b8d600a2f4d4aebca243507e9bbe97deaacfc4
ppershing <ppershing@c9fae5e5-1018-74da-50a3-0de90486fdfd>
2011-03-13 14:36:42 +0000

    Kompilator najnovsie hadze warningy a linkuje staticky kvoli testeru.
    git-svn-id: http://ksp-hell-world.googlecode.com/svn@13 c9fae5e5-1018-74da-50a3-0de90486fdfd

--- testovac/compiler/compile.sh
+++ testovac/compiler/compile.sh
@@ -6,11 +6,11 @@ fi
 filename="$1"
 if [ "${filename: -6}" == ".hellc" ]; then
     cat "$filename" | ./preprocess_cpp.sh > __.cpp;
-    g++ __.cpp -o hell 2> __.err ||
+    g++ -static -W -Wall __.cpp -o hell 2> __.err ||
       (cat __.err | ./recover_errormsg.sh && exit 1)
 elif [ "${filename: -6}" == ".hellp" ]; then
     cat "$filename" | ./preprocess_pas.sh > __.pas;
-    fpc __.pas -ohell 2> __.err ||
+    fpc -ve -iwn __.pas -ohell 2> __.err ||
       (cat __.err | ./recover_errormsg.sh && exit 1)
 else
     echo "Unknown extension";
