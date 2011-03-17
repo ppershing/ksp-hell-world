8923ff32629e45a1389988d5cba75a371dcfd9f8
ppershing <ppershing@c9fae5e5-1018-74da-50a3-0de90486fdfd>
2011-03-17 09:39:02 +0000

    Novy compile skript pusta veci pomocou bashu (netreba menit prava na .sh)
    git-svn-id: http://ksp-hell-world.googlecode.com/svn@28 c9fae5e5-1018-74da-50a3-0de90486fdfd

--- testovac/compiler/compile.sh
+++ testovac/compiler/compile.sh
@@ -5,13 +5,13 @@ fi
 
 filename="$1"
 if [ "${filename: -6}" == ".hellc" ]; then
-    cat "$filename" | ./preprocess_cpp.sh > __.cpp;
+    cat "$filename" | bash ./preprocess_cpp.sh > __.cpp;
     g++ -static -W -Wall __.cpp -o hell 2> __.err ||
-      (cat __.err | ./recover_errormsg.sh && exit 1)
+      (cat __.err | bash ./recover_errormsg.sh && exit 1)
 elif [ "${filename: -6}" == ".hellp" ]; then
-    cat "$filename" | ./preprocess_pas.sh > __.pas;
+    cat "$filename" | bash ./preprocess_pas.sh > __.pas;
     fpc -ve -iwn __.pas -ohell 2> __.err ||
-      (cat __.err | ./recover_errormsg.sh && exit 1)
+      (cat __.err | bash ./recover_errormsg.sh && exit 1)
 else
     echo "Unknown extension";
     exit 1;
