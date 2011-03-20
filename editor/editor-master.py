8d210e2614180e6e44bf24d9501f9cabb599de8b
mino <mino@c9fae5e5-1018-74da-50a3-0de90486fdfd>
2011-03-20 20:58:11 +0000

    Added master process supervising editor
    git-svn-id: http://ksp-hell-world.googlecode.com/svn@76 c9fae5e5-1018-74da-50a3-0de90486fdfd

---
+++ editor/editor-master.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python
+
+import re
+import os
+import gtk
+import glib
+import webkit
+import signal
+import os.path
+import gobject
+import threading
+log_path = './log.html'
+tasks_path = '../testovac/tasks/'
+done_dir = './done/'
+saves_dir = './saves/'
+last_update = 0
+n_choices = 2
+
+os.environ['SDL_VIDEO_WINDOW_POS'] = '0,0'
+
+gtk.threads_init ()
+
+def launch_editor (task, lang):
+	gobject.idle_add (tc_window.hide_all)
+	quit = False
+	while not quit:
+		quit = True
+		if lang == 'HellC':
+			ext ='.hellc'
+		elif lang == 'HellP':
+			ext = '.hellp'
+		file_path = os.path.join(saves_dir, task + ext)
+		ret = os.system ('./editor.py %s' % file_path)
+		ret = ret >> 8
+		print "got "+str(ret)
+		if False: pass
+		elif ret == 0:
+			# Normal exit
+			pass
+		elif ret == 1:
+			# Solution was accepted 
+			os.system ("touch '%s'" % os.path.join (done_dir, task))
+		elif ret == 47:
+			# expected failure, return quietly as if it was 0
+			pass
+		else:
+			# crash ? Try reloading last state
+			quit = False
+	gobject.idle_add (tc_window_restore)
+		
+def tc_window_restore(*args):
+	update_task_buttons ()
+	tc_window.show_all ()
+	tc_window.move (0, 0)
+
+def refresh_log ():
+	log = open (log_path).read()
+	if log.find('<body') == -1:
+		log = '<body bgcolor="black" style="color: white">' + log + '</body>'
+		log = log.replace (chr(10), '<br/>')
+	log_webkit.load_html_string (log,base_uri='.')
+
+def update_view ():
+	global last_update
+
+	# test change of logfile
+	try:
+		current_update = os.stat (log_path).st_mtime
+
+		if last_update < current_update:
+			last_update = current_update
+			refresh_log ()
+	except OSError:
+		pass
+	return True
+
+def get_available_tasks ():
+	available_tasks = task_list
+	solved_tasks = filter (is_task, map(os.path.basename,	os.listdir(done_dir)))
+
+	for task in solved_tasks:
+		if task in available_tasks:
+			available_tasks.remove (task)
+	
+	return available_tasks[0:n_choices]
+
+def is_task (x):
+	return re.match (r'^[0-9][0-9].*$', x) != None
+
+def update_task_buttons ():
+	current_tasks = get_available_tasks ()
+	for i in range(0, n_choices):
+		tc_tasks[i].set_label (current_tasks[i])
+
+
+def task_selected (sender):
+	update_task_buttons ()
+	selected_task = sender.get_label()
+	selected_language = tc_language.get_active_text()
+	task_desc = open (os.path.join (tasks_path, selected_task, 'desc')).read()
+	task_webkit.load_html_string (task_desc, base_uri= '.')
+	file(log_path,'w+').write('')
+
+	print selected_task, selected_language 
+	threading.Thread (target = launch_editor, args = (selected_task, selected_language)).start()
+
+
+os.unlink (log_path)
+
+# load current state
+
+task_list = filter(is_task, map(os.path.basename, os.listdir(tasks_path)))
+task_list.sort ()
+
+print get_available_tasks ()
+
+# log window
+log_window = gtk.Window ()
+log_scrollarea = gtk.ScrolledWindow ()
+log_webkit = webkit.WebView ()
+log_scrollarea.add (log_webkit)
+log_window.add (log_scrollarea)
+
+# task description window
+task_window = gtk.Window ()
+task_scrollarea = gtk.ScrolledWindow ()
+task_webkit = webkit.WebView ()
+task_scrollarea.add (task_webkit)
+task_window.add (task_scrollarea)
+
+# task choice window
+tc_language = gtk.combo_box_new_text ()
+
+langs = ['HellC', 'HellP']
+for i in langs:
+	#it = options.append ()
+	#options.set (it, 0, i)
+	tc_language.append_text (i)
+tc_language.set_active (0)
+
+tc_window = gtk.Window ()
+tc_vbox = gtk.VBox (homogeneous = False)
+tc_vbox.set_homogeneous (False)
+tc_tasks = []
+for i in range (0, n_choices):
+	tc_tasks += [ gtk.Button() ]
+	vbox = gtk.VBox ()
+	tc_tasks[-1].connect ('clicked', task_selected)
+
+tc_vbox.pack_start (tc_language, expand = False)
+tc_btnbox = gtk.HBox ()
+for i in tc_tasks: tc_btnbox.add (i)
+tc_vbox.pack_start (tc_btnbox)
+tc_window.add (tc_vbox)
+
+log_window.show_all ()
+task_window.show_all ()
+tc_window.show_all ()
+
+update_task_buttons ()
+
+# Now, find out dimensions of the screen; assuming we run on one screen
+screen = log_window.get_screen ()
+width = screen.get_width ()
+height = screen.get_height ()
+
+log_window.resize (width, 240)
+task_window.resize (width-640, height - 320)
+tc_window.resize (640,480)
+
+log_window.move (0, width - 240)
+task_window.move (640, 0)
+tc_window.move (0,0)
+
+glib.timeout_add (100, update_view)
+log_window.connect ('destroy', gtk.main_quit)
+task_window.connect ('destroy', gtk.main_quit)
+tc_window.connect ('destroy', gtk.main_quit )
+
+gtk.main()
