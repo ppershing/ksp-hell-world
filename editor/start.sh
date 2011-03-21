719ad11196ddac3d75c10d0530d1d747869f52b3
mino <mino@c9fae5e5-1018-74da-50a3-0de90486fdfd>
2011-03-21 13:57:54 +0000

    Added startup script

---
+++ editor/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# launch audio
+
+if [ -s mplayer.pid ]; then
+	kill $(cat mplayer.pid)
+fi
+
+mplayer -volume 50 -softvol -loop 0 -shuffle soundtrack/*.ogg soundtrack/*.mp3  >/dev/null 2>&1 &
+echo "$!" >mplayer.pid
+
+while true; do
+	./editor-master.py
+	sleep 1
+done
+
